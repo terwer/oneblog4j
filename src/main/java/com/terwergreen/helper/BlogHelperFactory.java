efc4126bce233af77c8d3955368495fa4f503931
Terwer <youweics@163.com>
2022-01-20 16:01:22 +0800

    集成httpclient与xmlrpc

--- src/main/java/com/terwergreen/helper/BlogHelperFactory.java
+++ src/main/java/com/terwergreen/helper/BlogHelperFactory.java
@@ -1,22 +1,40 @@
 package com.terwergreen.helper;
 
+import com.terwergreen.App;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * @author: terwer
  * @date: 2022/1/9 18:53
  * @description: BlogHelperFactory
  */
 public class BlogHelperFactory {
-    private BlogHelperFactory(){
+
+    private static Properties blogProps = new Properties();
+
+    static {
+        InputStream blogPropsStream = App.class.getClassLoader().getResourceAsStream("blog.properties");
+        try {
+            blogProps.load(blogPropsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private BlogHelperFactory() {
 
     }
 
-    public static BlogHelper getBlogHelper(BlogTypeEnum blogType){
-        switch (blogType){
-            case BUGUCMS:{
-                return new BuguCMSHelper();
+    public static BlogHelper getBlogHelper(BlogTypeEnum blogType) {
+        switch (blogType) {
+            case BUGUCMS: {
+                return new BuguCMSHelper(blogProps);
             }
-            case CNBLOGS:{
-                return new CnblogsHelper();
+            case CNBLOGS: {
+                return new CnblogsHelper(blogProps);
             }
             default:
                 return null;
